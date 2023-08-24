43389a3d8eb24679d20e37d91de6be20c46673c9
Alywcs <56103017+Alywcs@users.noreply.github.com>
2023-08-24 15:01:08 +0800

    Delete app directory

--- app/UseContext.js
+++
@@ -1,7 +0,0 @@
-// Allows components to access shared data provided by parent component, no need prop
-
-import { createContext } from 'react';
-
-const UseContext = createContext();
-
-export default UseContext;
